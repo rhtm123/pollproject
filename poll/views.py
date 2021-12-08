2c0c1470394a75b62a22d1c83bc2d0965859bc9c
rhtm123 <maurya.iitk@gmail.com>
2021-12-08 11:38:47 +0530

    poll extended

--- poll/views.py
+++ poll/views.py
@@ -11,10 +11,28 @@ def poll_view(request, pk):
         que = Question.objects.get(id=pk)
         choices = Choice.objects.filter(question=que)
 
+        if request.method == "POST":
+            selected_choice_id = request.POST.get("choice")
+            selected_choice = Choice.objects.get(id=selected_choice_id)
+            selected_choice.vote_count = selected_choice.vote_count + 1
+            selected_choice.save()
+            return render(
+                request,
+                "poll.html",
+                {
+                    "selected_choice": selected_choice,
+                    "submitted": True,
+                    "question": que,
+                    "choices": choices,
+                },
+            )
+
     except:
         return render(request, "404.html")
 
-    return render(request, "poll.html", {"question": que, "choices": choices})
+    return render(
+        request, "poll.html", {"submitted": False, "question": que, "choices": choices}
+    )
 
 
 def home_view(request):
